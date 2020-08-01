a79d7b381330ca005289e5c0638b8f0e77bc3816
teefats <30376635+teefats@users.noreply.github.com>
2020-08-01 12:46:35 +0100

    This is a trial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+// step 2 import homepage function from homepage.component.jsx
+import HomePage from './homepage.component'
+
+
+// step 3 render homepage by inserting in APP
+function App() {
+  return (
+    <div>
+    <HomePage />
+    </div>
+  );
+}
+
+export default App;
